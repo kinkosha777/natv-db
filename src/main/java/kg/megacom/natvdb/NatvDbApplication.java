b87f5979b2ae9fd7fd6c2f99dd0ba2f108a42af3
Bekmyrza Rakhimberdiev <funtik.kgz312@gmail.com>
2021-07-21 19:45:28 +0600

    Initial commit

---
+++ src/main/java/kg/megacom/natvdb/NatvDbApplication.java
@@ -0,0 +1,13 @@
+package kg.megacom.natvdb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NatvDbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NatvDbApplication.class, args);
+	}
+
+}
